09881dff687ceb190e3fc2f272d639e1f5bf9007
XptNG358 <arsh013@hotmail.com>
2020-02-04 10:49:55 -0500

    Lab 1 Solutions

---
+++ MyAgels.java
@@ -0,0 +1,16 @@
+public class MyAgels {
+
+    public static void main(String[] args) {
+        
+        int currentYear, yearOfBirth, age;
+
+        currentYear = 2020;
+        yearOfBirth = 2001;
+        age = currentYear - yearOfBirth;
+
+        // Prints "Soon I will be 19 years old" to the terminal window.
+        System.out.println("\n! Soon I will be " + age + " years old !");
+
+    }
+
+}
